21e771e3858bee304c3abf2476aa28b327d6aef4
Daniel Frett <daniel.frett@ccci.org>
2014-04-15 11:49:00 -0400

    prefer using built-in packages when possible

--- api/src/test/java/org/ccci/gto/globalreg/TestUtils.java
+++ api/src/test/java/org/ccci/gto/globalreg/TestUtils.java
@@ -1,14 +1,15 @@
 package org.ccci.gto.globalreg;
 
-import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class TestUtils {
     public static String loadResource(final Class<?> clazz, final String name) throws IOException {
-        try (final InputStreamReader in = new InputStreamReader(clazz.getResourceAsStream(name), Charsets.UTF_8)) {
+        try (final InputStreamReader in = new InputStreamReader(clazz.getResourceAsStream(name),
+                StandardCharsets.UTF_8)) {
             return CharStreams.toString(in);
         }
     }
