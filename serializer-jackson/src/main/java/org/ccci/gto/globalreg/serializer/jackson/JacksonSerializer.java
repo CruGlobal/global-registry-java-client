023f1f48e7aa2e71a24b01ec59bc14b46f419c29
Daniel Frett <daniel.frett@ccci.org>
2014-04-21 14:18:33 -0400

    added Jackson POJO serializer

---
+++ serializer-jackson/src/main/java/org/ccci/gto/globalreg/serializer/jackson/JacksonSerializer.java
@@ -0,0 +1,78 @@
+package org.ccci.gto.globalreg.serializer.jackson;
+
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Throwables;
+import org.ccci.gto.globalreg.EntityType;
+import org.ccci.gto.globalreg.ResponseList;
+import org.ccci.gto.globalreg.serializer.AbstractSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class JacksonSerializer extends AbstractSerializer {
+    private static final Logger LOG = LoggerFactory.getLogger(JacksonSerializer.class);
+
+    private final ObjectMapper mapper;
+
+    public JacksonSerializer() {
+        this.mapper = new ObjectMapper();
+        this.mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public JacksonSerializer(final ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public <T> T parseEntity(final EntityType<T> type, final String raw) {
+        try {
+            final JsonNode json = mapper.readTree(raw);
+            return mapper.treeToValue(json.path("entity").path(type.getEntityType()), type.getEntityClass());
+        } catch (final IOException e) {
+            LOG.error("Unexpected IOException", e);
+            throw Throwables.propagate(e);
+        }
+    }
+
+    @Override
+    public <T> ResponseList<T> parseEntitiesList(final EntityType<T> type, final String raw) {
+        try {
+            final JsonNode root = mapper.readTree(raw);
+            final ResponseList<T> list = new ResponseList<>();
+
+            // parse all returned entities
+            final JsonNode entities = root.path("entities");
+            if(entities.isArray()) {
+                for(final JsonNode entity : entities) {
+                    list.add(mapper.treeToValue(entity.path(type.getEntityType()), type.getEntityClass()));
+                }
+            }
+
+            // parse the meta-data
+            final JsonNode metaJson = root.path("meta");
+            final ResponseList.Meta meta = list.getMeta();
+            meta.setTotal(metaJson.path("total").asInt(0));
+            meta.setFrom(metaJson.path("from").asInt(0));
+            meta.setTo(metaJson.path("to").asInt(0));
+            meta.setPage(metaJson.path("page").asInt(0));
+            meta.setTotalPages(metaJson.path("total_pages").asInt(0));
+
+            // return the parsed list
+            return list;
+        } catch (final IOException e) {
+            LOG.error("Unexpected IOException", e);
+            throw Throwables.propagate(e);
+        }
+    }
+
+    @Override
+    public <T> String fromObject(final EntityType<T> type, final T object) {
+        //TODO
+        return null;
+    }
+}
---
+++ serializer-jackson/src/test/java/org/ccci/gto/globalreg/serializer/jackson/JacksonSerializerTest.java
@@ -0,0 +1,148 @@
+package org.ccci.gto.globalreg.serializer.jackson;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.ccci.gto.globalreg.EntityType;
+import org.ccci.gto.globalreg.ResponseList;
+import org.ccci.gto.globalreg.serializer.AbstractSerializerTest;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class JacksonSerializerTest extends AbstractSerializerTest {
+    public JacksonSerializerTest() {
+        super(new JacksonSerializer());
+    }
+
+    private static final Collection<Class<? extends Person>> CLASSES = new HashSet<>();
+
+    static {
+        CLASSES.add(PersonField.class);
+        CLASSES.add(PersonMethod.class);
+    }
+
+    @Test
+    public void testParseEntity() throws Exception {
+        for (final Class<? extends Person> clazz : CLASSES) {
+            final Person person = this.testParseEntity(new EntityType<>(clazz, "person"));
+            assertEquals(5, (int) person.getId());
+            assertEquals("John", person.getFirstName());
+            assertEquals("Doe", person.getLastName());
+            assertEquals("Ohio University", person.getCampus());
+        }
+    }
+
+    @Test
+    public void testParseEntitiesList() throws Exception {
+        for (final Class<? extends Person> clazz : CLASSES) {
+            final ResponseList<Person> entities = this.testParseEntitiesList(new EntityType<>(clazz, "person"));
+
+            // all records should have a last_name of Vellacott
+            for (final Person entity : entities) {
+                assertEquals("Vellacott", entity.getLastName());
+            }
+        }
+    }
+
+    private abstract static class Person {
+        protected abstract Integer getId();
+
+        protected abstract void setId(final Integer id);
+
+        protected abstract String getFirstName();
+
+        protected abstract void setFirstName(final String name);
+
+        protected abstract String getLastName();
+
+        protected abstract void setLastName(final String name);
+
+        protected abstract String getCampus();
+
+        protected abstract void setCampus(final String campus);
+    }
+
+    public static class PersonField extends Person {
+        public Integer id;
+        @JsonProperty("first_name")
+        public String firstName;
+        @JsonProperty("last_name")
+        public String lastName;
+        public String campus;
+
+        protected Integer getId() {
+            return this.id;
+        }
+
+        protected void setId(final Integer id) {
+            this.id = id;
+        }
+
+        protected String getFirstName() {
+            return firstName;
+        }
+
+        protected void setFirstName(final String name) {
+            this.firstName = name;
+        }
+
+        protected String getLastName() {
+            return this.lastName;
+        }
+
+        protected void setLastName(final String name) {
+            this.lastName = name;
+        }
+
+        protected String getCampus() {
+            return campus;
+        }
+
+        protected void setCampus(final String campus) {
+            this.campus = campus;
+        }
+    }
+
+    public static class PersonMethod extends Person {
+        private Integer id;
+        private String firstName;
+        private String lastName;
+        private String campus;
+
+        public Integer getId() {
+            return this.id;
+        }
+
+        public void setId(final Integer id) {
+            this.id = id;
+        }
+
+        @JsonProperty("first_name")
+        public String getFirstName() {
+            return this.firstName;
+        }
+
+        public void setFirstName(final String name) {
+            this.firstName = name;
+        }
+
+        public String getLastName() {
+            return this.lastName;
+        }
+
+        @JsonProperty("last_name")
+        public void setLastName(final String name) {
+            this.lastName = name;
+        }
+
+        public String getCampus() {
+            return this.campus;
+        }
+
+        public void setCampus(final String campus) {
+            this.campus = campus;
+        }
+    }
+}
