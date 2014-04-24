a68e7022589770492dd5af03f21f320a7c9c0613
Daniel Frett <daniel.frett@ccci.org>
2014-04-24 13:10:51 -0400

    make the Filter cleass serializable and usable with equals and hashCode

--- api/src/main/java/org/ccci/gto/globalreg/Filter.java
+++ api/src/main/java/org/ccci/gto/globalreg/Filter.java
@@ -1,6 +1,11 @@
 package org.ccci.gto.globalreg;
 
-public final class Filter {
+import java.io.Serializable;
+import java.util.Arrays;
+
+public final class Filter implements Serializable {
+    private static final long serialVersionUID = 1900099970741635758L;
+
     private final String[] path;
     private final String value;
 
@@ -9,8 +14,8 @@ public final class Filter {
     }
 
     public Filter(final String[] path, final String value) {
-        this.path = path;
-        this.value = value;
+        this.path = path != null ? path : new String[0];
+        this.value = value != null ? value : "";
     }
 
     public final Filter path(final String... path) {
@@ -31,5 +36,21 @@ public final class Filter {
 
     public final String getValue() {
         return this.value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Filter filter = (Filter) o;
+        return Arrays.equals(this.path, filter.path) && this.value.equals(filter.value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(path);
+        result = 31 * result + value.hashCode();
+        return result;
     }
 }
