2d62007424a87140bdae49f8ee45a6ead3c84ccb
ryancarlson <ryan.t.carlson@cru.org>
2014-06-30 13:11:32 -0400

    Buildout first pass at Resteasy Client

--- resteasyclient/src/main/java/org/ccci/gto/globalreg/resteasy/ResteasyGlobalRegistryClient.java
+++ resteasyclient/src/main/java/org/ccci/gto/globalreg/resteasy/ResteasyGlobalRegistryClient.java
@@ -5,6 +5,8 @@ import org.ccci.gto.globalreg.UnauthorizedException;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 
 /**
@@ -17,12 +19,63 @@ public class ResteasyGlobalRegistryClient extends BaseGlobalRegistryClient
 	protected Response processRequest(Request request) throws UnauthorizedException
 	{
 
-		return null;
+		Invocation.Builder requestBuilder = webTarget()
+				.path(buildPath(request))
+				.queryParam("access_token", accessToken)
+				.request();
+
+		javax.ws.rs.core.Response resteasyResponse = execute(requestBuilder, request);
+
+		return buildResponse(resteasyResponse);
+	}
+
+	private Response buildResponse(javax.ws.rs.core.Response resteasyResponse)
+	{
+			return new Response(resteasyResponse.getStatus(), resteasyResponse.readEntity(String.class));
 	}
 
 	private WebTarget webTarget()
 	{
 		Client client = ClientBuilder.newBuilder().build();
 		return client.target(apiUrl);
+	}
+
+	private String buildPath(Request request)
+	{
+		StringBuilder pathBuilder = new StringBuilder();
+
+		for(String pathElement : request.path)
+		{
+			pathBuilder.append(pathElement);
+
+		}
+
+		return pathBuilder.toString();
+	}
+
+	private javax.ws.rs.core.Response execute(Invocation.Builder requestBuilder, Request request)
+	{
+		if("GET".equalsIgnoreCase(request.method))
+		{
+			return requestBuilder.get();
+		}
+		if("POST".equalsIgnoreCase(request.method))
+		{
+			return requestBuilder.post(Entity.json(request.content));
+		}
+		if("PUT".equalsIgnoreCase(request.method))
+		{
+			return requestBuilder.put(Entity.json(request.content));
+		}
+		if("DELETE".equalsIgnoreCase(request.method))
+		{
+			return requestBuilder.delete();
+		}
+		if("OPTIONS".equalsIgnoreCase(request.method))
+		{
+			return requestBuilder.options();
+		}
+
+		throw new UnsupportedOperationException();
 	}
 }
