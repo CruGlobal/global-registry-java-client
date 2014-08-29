f5ae551bcde4361e2c7f68b83fe7109af9f5268e
ryancarlson <ryan.t.carlson@cru.org>
2014-08-29 15:51:36 -0400

    `UnauthorizedException` now extends `ClientErrorException`

--- api/src/main/java/org/ccci/gto/globalreg/UnauthorizedException.java
+++ api/src/main/java/org/ccci/gto/globalreg/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package org.ccci.gto.globalreg;
 
-public class UnauthorizedException extends GlobalRegistryException {
+public class UnauthorizedException extends ClientErrorException {
     private static final long serialVersionUID = -226761129706293401L;
+
+	public UnauthorizedException() {
+		super(401, "Unauthorized.  Access token was missing or invalid");
+	}
 }
