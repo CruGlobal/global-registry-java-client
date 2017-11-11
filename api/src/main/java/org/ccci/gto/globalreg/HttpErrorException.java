b64b98f978f0f1beb3ca24088c7ffebe3ad777fc
Matt Drees <matt.drees@cru.org>
2017-11-10 22:10:20 -0700

    nicer error message rendering
    In the common case, errors from GR are a simple json payload with a single 'error' key/value pair. For these, let's strip off the json encoding.
    
    For other errors (perhaps ELB-issued ones), the format of the error message is not very predictable, so we print the message as is (but truncated).
    
    This uses a regex for json parsing because this project is intended to be json-parsing-implementation agnostic, and I didn't feel it was appropriate to add this very small parsing task to the Serializer API.

--- api/src/main/java/org/ccci/gto/globalreg/HttpErrorException.java
+++ api/src/main/java/org/ccci/gto/globalreg/HttpErrorException.java
@@ -2,14 +2,49 @@ package org.ccci.gto.globalreg;
 
 import com.google.common.base.Ascii;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class HttpErrorException extends GlobalRegistryException {
+
+    private static final Pattern JSON_ERROR_PATTERN;
+    static {
+        String whitespace = "\\s*";
+        JSON_ERROR_PATTERN = Pattern.compile(
+                "\\{" +
+                whitespace +
+                "\"error\"" +
+                whitespace +
+                ":" +
+                whitespace +
+                "\"(?<errorMessage>.*)\"" +
+                whitespace +
+                "}"
+        );
+    }
+
     final int statusCode;
     final String responseContent;
 
     public HttpErrorException(int statusCode, String responseContent) {
-        super(String.format("status code %s: %s", statusCode, Ascii.truncate(responseContent, 200, "...")));
+        super(determineMessage(statusCode, responseContent));
         this.statusCode = statusCode;
         this.responseContent = responseContent;
+    }
+
+    private static String determineMessage(final int statusCode, final String responseContent) {
+        String error = getJsonErrorMessageIfPossible(responseContent);
+        if (error != null) {
+            return String.format("status code %s: %s", statusCode, error);
+        } else {
+            // most likely an html response
+            return String.format("status code %s: %n%s", statusCode, Ascii.truncate(responseContent, 200, "…"));
+        }
+    }
+
+    private static String getJsonErrorMessageIfPossible(final String responseContent) {
+        Matcher matcher = JSON_ERROR_PATTERN.matcher(responseContent);
+        return matcher.matches() ? matcher.group("errorMessage") : null;
     }
 
     public HttpErrorException(Throwable cause, int statusCode, String responseContent) {
