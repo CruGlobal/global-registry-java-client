f20c2a48c4f699429713ca159606dc5029cafbc1
Matt Drees <matt.drees@cru.org>
2017-11-16 15:57:59 -0700

    remove redundant getters

--- api/src/main/java/org/ccci/gto/globalreg/ServerErrorException.java
+++ api/src/main/java/org/ccci/gto/globalreg/ServerErrorException.java
@@ -8,12 +8,4 @@ public class ServerErrorException extends HttpErrorException {
 	public ServerErrorException(int statusCode, String responseContent) {
 		super(statusCode, responseContent);
 	}
-
-	public int getStatusCode() {
-		return statusCode;
-	}
-
-	public String getResponseContent() {
-		return responseContent;
-	}
 }
