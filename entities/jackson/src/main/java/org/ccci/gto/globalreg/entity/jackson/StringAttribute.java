8953c9b1410e237fc9003532ad55e3f1864cefe7
Bill Randall <william.randall@cru.org>
2018-02-09 09:38:00 -0500

    Add 3 common attribute types for use by clients reading from multiple owner systems

---
+++ entities/jackson/src/main/java/org/ccci/gto/globalreg/entity/jackson/StringAttribute.java
@@ -0,0 +1,65 @@
+package org.ccci.gto.globalreg.entity.jackson;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.Date;
+
+/**
+ * Entity representing a String value when querying with multiple owned_by systems.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "value",
+    "created_by",
+    "updated_at",
+    "user_entered"
+})
+public class StringAttribute {
+    @JsonProperty("value")
+    private String value;
+
+    @JsonProperty("created_by")
+    private String createdBy;
+
+    @JsonProperty("updated_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date updatedAt;
+
+    @JsonProperty("user_entered")
+    private Boolean userEntered;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getUserEntered() {
+        return userEntered;
+    }
+
+    public void setUserEntered(Boolean userEntered) {
+        this.userEntered = userEntered;
+    }
+}
